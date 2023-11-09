de464994e4353db2a370bf958e705fe968cf0e8d
Socumo <alosocumo363@gmail.com>
2023-11-09 15:37:06 -0600

    modified function playRound

--- script/javascript.js
+++ script/javascript.js
@@ -8,54 +8,56 @@ function playRound(playerSelection, computerSelection) {
 
         return (`A Tie! ${playerSelection} and ${computerSelection}`); 
 
-    }else if (playerSelection === "scissors" && computerSelection === "rock"  || 
-              playerSelection === "rock" && computerSelection === "paper"  || 
-              playerSelection === "paper" && computerSelection === "scissors") {
-
-        return (`You lose! ${playerSelection} beats ${computerSelection}`); 
-
-    }else if (playerSelection === "scissors" && computerSelection === "paper"  || 
-              playerSelection === "rock" && computerSelection === "scissors"  || 
-              playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper"  || 
+              playerSelection === "paper" && computerSelection === "rock"  || 
+              playerSelection === "rock" && computerSelection === "scissors") {
 
         return (`You win! ${playerSelection} beats ${computerSelection}`); 
+
+    } else  {
+
+        return (`You lose! ${computerSelection} beats ${playerSelection}`); 
 
         }
     
   }
-  playRound();
  
   function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
+    console.log("rn>" + randomNumber);
     let computerChoice = "";
 
+
     switch (randomNumber) {
-    case 1:
+    case 0:
         computerChoice ='rock';
-    case 2:
+        break;
+    case 1:
         computerChoice= 'paper';
-    case 3:
+        break;
+    case 2:
         computerChoice=  'scissors';
+        break;
     }
 
     return computerChoice;
 
     }
-    getComputerChoice();
 
     function game(playerSelect){
 
         let playerSelection = prompt("Write rock, scissors or paper");
         playerSelect = playerSelection;     
         let computerChoice = getComputerChoice();   
+        console.log("cc>" +  computerChoice);
         let result = playRound(playerSelect,computerChoice); 
+        console.log("result>" +  result);
         const game = document.querySelector("#game");
       
         game.textContent = `Your choice was  ${playerSelection} and 
         computer choices ${computerChoice} so ${result} `;
         
-        console.log(result); 
-
+        
 
     }
     button.addEventListener("click", game);
